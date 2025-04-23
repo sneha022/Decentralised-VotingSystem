1d6c7f30e6a0291e0b29d2d38c043a08cd9b05a6
Bhavani <22wh1a05a9@bvrithyderabad.edu.in>
2025-04-23 15:50:33 +0530

    Files Added

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import WelcomePage from './components/WelcomePage';
 import StudentLogin from './components/StudentLogin';
 import StudentNomination from './components/StudentNomination';
-import OTPVerification from './components/OTPVerification';
-import Success from './components/Success';
-import VerificationSent from './components/VerificationSent'; // ✅ ADD THIS IMPORT
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<WelcomePage />} />
       <Route path="/login" element={<StudentLogin />} />
-      <Route path="/otp-verification" element={<OTPVerification />} />
       <Route path="/nominations" element={<StudentNomination />} />
-      <Route path="/success" element={<Success />} />
-      <Route path="/verification-sent" element={<VerificationSent />} /> {/* ✅ ADD THIS ROUTE */}
     </Routes>
   </Router>
 );
